6cd5f9a6d9ccf088480800336d829d21f6b02857
Georgi Grigorov <joro.grigorov@gmail.com>
2022-10-19 17:42:22 +0300

    add static data for buttons

---
+++ data/buttons.js
@@ -0,0 +1,18 @@
+const ButtonsData = [
+  {
+    text: 'Email',
+    icon: 'fa-solid fa-envelope',
+    isLink: true,
+    target: '_blank',
+    href: '#'
+  },
+  {
+    text: 'LinkedIn',
+    icon: 'fa-brands fa-linkedin',
+    isLink: true,
+    target: '_blank',
+    href: '#'
+  },
+]
+
+export default ButtonsData
